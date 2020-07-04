434b35c5325e1be70e109097de907b86578f2e02
Meher Kasam <meher@squareup.com>
2020-07-03 19:23:19 -0700

    GH Pages Configuration

---
+++ build/precache-manifest.775605e5c132fffd1048a9a10d8dd4ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3b20142b9f68e5148dbad8b1ba2eb2",
+    "url": "/KDF-Website/index.html"
+  },
+  {
+    "revision": "aad42f6770b1dbcfbf29",
+    "url": "/KDF-Website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "02e175b21263c7328a11",
+    "url": "/KDF-Website/static/js/2.02806a0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KDF-Website/static/js/2.02806a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad42f6770b1dbcfbf29",
+    "url": "/KDF-Website/static/js/main.7bf1aa0d.chunk.js"
+  },
+  {
+    "revision": "237c6c2ac05fcae28f29",
+    "url": "/KDF-Website/static/js/runtime-main.c5c4c8f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/KDF-Website/static/media/logo.5d5d9eef.svg"
+  }
+]);
