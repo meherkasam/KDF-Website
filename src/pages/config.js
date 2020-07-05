d530e197673db93c2268cea358283f29301eb04c
Meher Kasam <meher@squareup.com>
2020-07-04 23:44:44 -0700

    Add endpoint config page

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from 'react'
 
-function Config() {
-    return (
-        <div>
-        <h2>Configuration</h2>
-        </div>
-    );
+class Config extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            endpoint: localStorage.getItem('endpoint')
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSave = this.handleSave.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({endpoint: event.target.value})
+    }
+
+    handleSave(event) {
+        localStorage.setItem('endpoint', this.state.endpoint)
+        event.preventDefault()
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Configuration</h1>
+                <form onSubmit={this.handleSave}>
+                    <label>
+                        Endpoint:
+                <input type="text" value={this.state.endpoint} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Save" />
+                </form>
+            </div>
+        )
+    }
 }
 
-export default Config;
+export default Config
